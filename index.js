ee4f4beccd72ac7278f020a16136bf5b34e7c718
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-07-12 14:51:40 +0000

    init

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// const input = document.getElementById ('button')
+// input.addEventListener("click", addingEventListener)
+
+// function addingEventListener() {
+//     console.log("hello")
+// }
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('Element clicked!');
+    });
 }
+
+// Call the function to set up the event listener
+addingEventListener();
